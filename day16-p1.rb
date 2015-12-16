08b9dfb1c39478ef2365dc357ff606bd35da8622
Laurent Dang <dang.laurent@gmail.com>
2015-12-16 10:50:00 -0500

    Add Day16

---
+++ day16-p1.rb
@@ -0,0 +1,20 @@
+target = {
+  children: 3,
+  cats: 7,
+  samoyeds: 2,
+  pomeranians: 3,
+  akitas: 0,
+  vizslas: 0,
+  goldfish: 5,
+  trees: 3,
+  cars: 2,
+  perfumes: 1
+}
+
+File.read("#{__FILE__.split('-').first}.txt").each_line do |line|
+  properties = Hash[*line.scan(/(\w+): (\d)/).flatten]
+  p line if properties.all? do |property, value|
+    value.to_i == target[property.to_sym]
+  end
+end
+
