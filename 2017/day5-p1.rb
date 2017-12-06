dd686f1e3c69b30a4d987a42c1c20af4df8ee893
Laurent Dang <dang.laurent@gmail.com>
2017-12-06 10:17:52 -0500

    2017 / Day 5

---
+++ 2017/day5-p1.rb
@@ -0,0 +1,22 @@
+require 'pry'
+inputs = if ARGV[0]
+  ARGV[0].split(' ').map(&:to_i)
+else
+  File.read('day5.txt').split("\n").map(&:to_i)
+end
+
+position = 0
+index = 0
+
+while true
+  index += 1
+  previous_position = position
+  position = inputs[position] + previous_position
+
+  if inputs[position].nil?
+    puts index
+    exit;
+  end
+
+  inputs[previous_position] = inputs[previous_position] + 1
+end
