24fcb5a21c7a5f4115e85dc6071a6d5c972dc693
Laurent Dang <dang.laurent@gmail.com>
2017-12-06 11:40:29 -0500

    2017 / Day 6

---
+++ 2017/day6-p1.rb
@@ -0,0 +1,33 @@
+inputs = if ARGV[0]
+  ARGV[0].split(' ').map(&:to_i)
+end
+
+states = [];
+state = inputs
+steps = 0
+
+while true
+  steps += 1
+  max_value = state.max
+  distribution = if state.max > state.length
+    (state.max / (state.length - 1)).floor
+  else
+    1
+  end
+  cursor = state.index(state.max)
+
+  state = state.rotate(cursor).each_with_index.map do |bank, i|
+    if i == 0
+      state.length > max_value ? 0 : 1
+    else
+      i <= state.max ? distribution + bank : bank
+    end
+  end.rotate(cursor * -1)
+
+  if states.include?(state)
+    puts steps
+    exit
+  else
+    states << state
+  end
+end
