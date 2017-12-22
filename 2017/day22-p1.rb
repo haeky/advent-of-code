c79dcd56e1177bae7a4515a8ad3130125e73e5fc
Laurent Dang <dang.laurent@gmail.com>
2017-12-22 12:08:49 -0500

    2017 / Day 22

---
+++ 2017/day22-p1.rb
@@ -0,0 +1,45 @@
+grid_size = 999
+
+input = File.read('day22.txt').each_line.map { |line| line.chomp.chars }
+grid = Array.new(grid_size) { Array.new(grid_size, '.') }
+
+directions = %i(left up right down)
+
+z = (grid.size/2) - (input.size/2)
+
+input.each_with_index do |y, i|
+  y.each_with_index do |_, j|
+    grid[z + i][z + j] = input[i][j]
+  end
+end
+
+x = (grid.size/2)
+y = (grid.size/2)
+
+direction = :up
+burst = 0
+
+10_000.times do
+  if grid[y][x] == '.'
+    direction = directions.rotate(directions.index(direction) - 1)[0]
+    grid[y][x] = '#'
+    burst += 1
+  else
+    direction = directions.rotate(directions.index(direction) - 1)[2]
+    grid[y][x] = '.'
+  end
+
+  case direction
+  when :left
+    x -= 1
+  when :up
+    y -= 1
+  when :right
+    x += 1
+  when :down
+    y += 1
+  end
+
+end
+
+puts burst
